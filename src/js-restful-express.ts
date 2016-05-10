ffe25e187822cdb941bec91b28a4146e7685168e
Michael Seemann <seemann@mseemann.de>
2016-05-10 19:13:38 +0200

    reexport js-restful api

--- src/js-restful-express.ts
+++ src/js-restful-express.ts
@@ -1,6 +1,24 @@
 export { ExpressServiceRegistry } from './express/service-registry';
 export { RenderWith, ExpressContext } from './express/decorators';
 export { ExpressContextType, ISecurityContextFactory } from './express/descriptions';
+// reexport js-restful to make usage easier.
+export {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    Path,
+    PathParam,
+    HeaderParam,
+    QueryParam,
+    Context,
+    PermitAll,
+    SecurityContext,
+    RolesAllowed,
+    HttpMethod,
+    ContextTypes,
+    ISecurityContext,
+    IUser } from 'js-restful';
 
 
 
